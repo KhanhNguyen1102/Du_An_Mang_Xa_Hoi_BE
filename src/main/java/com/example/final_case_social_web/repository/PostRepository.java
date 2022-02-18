76d0d864fa915c39d386e85098d21d6a0e41dd2f
binhkk3 <binhduongkk@gmail.com>
2022-02-18 10:12:39 +0700

    full

--- src/main/java/com/example/final_case_social_web/repository/PostRepository.java
+++ src/main/java/com/example/final_case_social_web/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.example.final_case_social_web.repository;
 
 import com.example.final_case_social_web.model.Post;
+import com.example.final_case_social_web.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
     Iterable<Post> findByStatusContaining (String status);
+    Iterable<Post> findAllByUserId(Long id);
     @Query(value = "select *from post where status =1 or status= 2",nativeQuery = true)
     Iterable<Post> findAllByStatusFriend();
     @Query(value = "select *from post where status =1",nativeQuery = true)
